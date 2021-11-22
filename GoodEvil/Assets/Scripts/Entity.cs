6370b9346a2a634366e46edad0c9009614831aed
Lucas de Souza <lucasdsm.personal@gmail.com>
2021-11-22 15:16:52 -0300

    Criando entidade e status

---
+++ GoodEvil/Assets/Scripts/Entity.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Entity : MonoBehaviour
+{
+    [Header("Informations")]
+    public new string name;
+    public string type;
+
+    [Header("Stats")]
+    public Stats stats;
+    void Start()
+    {
+        
+    }
+    
+}
---
+++ GoodEvil/Assets/Scripts/Stats.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stats : MonoBehaviour
+{
+    [Header("Health")]
+    public float currentHealth;
+    public float maxHealth;
+
+    [Header("Mana")]
+    public float currentMana;
+    public float maxMana;
+
+    [Header("Stamina")]
+    public float currentStamina;
+    public float maxStamina;
+
+    [Header("Stats")]
+    public float speed = 5;
+    public int power;
+    public int intelligence;
+    public int defense;
+    public int level;
+    public int xp;
+
+    public void Damage(float value)
+    {
+        currentHealth -= value;
+
+        if (currentHealth < 0) currentHealth = 0;
+    }
+
+    public void Heal(float value)
+    {
+        currentHealth += value;
+
+        if (currentHealth > maxHealth) currentHealth = maxHealth;
+    }
+
+
+}
+
