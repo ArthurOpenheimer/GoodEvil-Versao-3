56b33749a8b8d649a4b3974d536b5f32da92225e
João <joaovictorgodoydasilva@gmail.com>
2021-11-24 10:18:27 -0300

    alteração no script de dialogo, adição dos colliders da cena da arena e corredor

--- GoodEvil/Assets/Scripts/DialogueTrigger.cs
+++ GoodEvil/Assets/Scripts/DialogueTrigger.cs
@@ -6,10 +6,21 @@ public class DialogueTrigger : MonoBehaviour
 {
 
 	public Dialogue dialogue;
-
-	public void TriggerDialogue()
-	{
-		FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+	private bool isTrigger;
+	 void OnTriggerEnter2D(Collider2D other)
+    {
+		isTrigger = true;
+    }
+     void OnTriggerExit2D(Collider2D other)
+    {
+		isTrigger = false;
 	}
+	void Update()
+    {
+		if (Input.GetKeyDown("e") && isTrigger==true)
+		{
+			FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+		}
+    }
 	//Trigger for the Dialogue
 }
