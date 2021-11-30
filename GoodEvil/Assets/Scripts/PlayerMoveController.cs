6282e197c9381d376c2972eb0da0c8c625283cbe
João <joaovictorgodoydasilva@gmail.com>
2021-11-29 22:07:28 -0300

    adição das animações de movimento

--- GoodEvil/Assets/Scripts/PlayerMoveController.cs
+++ GoodEvil/Assets/Scripts/PlayerMoveController.cs
@@ -7,11 +7,13 @@ public class PlayerMoveController : MonoBehaviour
     public Rigidbody2D rb;
     public Entity entity;
     private Vector2 movement;
+    public Animator animator;
 
     void Start()
     {
         entity = GetComponent<Entity>();
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     void Update()
@@ -20,6 +22,30 @@ public class PlayerMoveController : MonoBehaviour
         movement.y = Input.GetAxisRaw("Vertical");
         //Set the maxValue of the Vector
         movement = Vector2.ClampMagnitude(movement, 1f);
+        animator.SetFloat("VelocityX", movement.x);
+        animator.SetFloat("VelocityY", movement.y);
+
+        if (movement == Vector2.zero)  
+        {
+            animator.SetBool("IsWalking", false);
+        }
+        else
+        {
+            animator.SetBool("IsWalking", true);
+        }
+       /* if (movement.x > 0 ) 
+        {
+            animator.SetFloat("Pos", 0);
+        }
+        else if(movement.x < 0)
+        {
+            animator.SetFloat("Pos", 2);
+        }
+
+        else if (movement.y < 0)
+        {
+            animator.SetFloat("Pos", 1);
+        }*/ 
     }
 
     void FixedUpdate()
